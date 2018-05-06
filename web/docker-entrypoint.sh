7bfc3170dd9199cb755a5914d104bdb99fbc2cf3
dzeyelid <dzeyelid@gmail.com>
2018-05-06 10:50:56 +0900

    Fixed environment variables for database settings.

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+sed -i -E "s/^(DB_HOST)=.*?$/\1=${DB_HOST}/g" ${WORK_DIR}/polls/.env
+sed -i -E "s/^(DB_DATABASE)=.*?$/\1=${DB_DATABASE}/g" ${WORK_DIR}/polls/.env
+sed -i -E "s/^(DB_USERNAME)=.*?$/\1=${DB_USERNAME}/g" ${WORK_DIR}/polls/.env
+sed -i -E "s/^(DB_PASSWORD)=.*?$/\1=${DB_PASSWORD}/g" ${WORK_DIR}/polls/.env
+
 service php7.2-fpm start
 exec nginx -g 'daemon off;'
